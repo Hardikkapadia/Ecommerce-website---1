e1e83cc69a4c272e73b336e2c946011d4e7131d8
Hardikkapadia <kapadiyahardik1@gmail.com>
2024-11-26 23:10:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-import { fileURLTOpath } from 'url';
 
 //configure env
 dotenv.config();
@@ -18,9 +17,6 @@ dotenv.config();
 connectDB();
 
 //esmodule fix
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
